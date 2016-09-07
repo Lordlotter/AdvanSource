ffb244304b797c52d1bbd4819d20232232be79c8
Lordlotter <Joojolord@Gmail.com>
2016-09-08 02:39:16 +0430

    Update note.lua

--- plugins/note.lua
+++ plugins/note.lua
@@ -29,8 +29,8 @@ return {
   "!mynote : show your note",
  },
  advan = {
-   "Created by: @janlou",
-   "Powered by: @AdvanTM",
+   "Created by: @lordlotter3",
+   "Powered by: @lordandroid_bot",
    "CopyRight all right reserved",
  },
  patterns = {
